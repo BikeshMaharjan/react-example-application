4120cd3387b2b7c906bd4cc9fd06eb55dc145739
Bikesh Maharjan <bikeshmaharjan@Bikeshs-MacBook-Air.local>
2023-03-09 21:46:57 -0500

    Search and Table component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Search from "./Search";
+import Table from "./Table";
+
+const objList = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
+  const [list, setList] = useState(objList);
+  const [searchTerm, setSearchTerm] = useState("");
+  const onDismiss = (id) => {
+    const isNotId = (item) => item.objectID !== id;
+    const updatedList = list.filter(isNotId);
+    setList(updatedList);
+  };
+  const onSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search value={searchTerm} onChange={onSearchChange}>
+        {" "}
+        Search{" "}
+      </Search>
+      <Table list={list} pattern={searchTerm} onDismiss={onDismiss} />
     </div>
   );
 }
