406a6965b80dbc57551c962cb3ce62baae675be2
Bikesh Maharjan <bikeshmaharjan@Bikeshs-MacBook-Air.local>
2023-03-16 22:46:28 -0400

    using client cache

--- src/App.js
+++ src/App.js
@@ -15,8 +15,12 @@ const PARAM_HPP = "hitsPerPage=";
 function App() {
   const [results, setResults] = useState(null);
   const [searchTerm, setSearchTerm] = useState(DEFAULT_QUERY);
-  const [page, setPage] = useState((results && results.page) || 0);
   const [searchKey, setSearchKey] = useState("");
+  const [page, setPage] = useState(
+    (results && results[searchKey] && results[searchKey].page) || 0
+  );
+
+  const list = (results && results[searchKey] && results[searchKey].hits) || [];
 
   useEffect(() => {
     fetchData(DEFAULT_PAGE);
@@ -24,15 +28,20 @@ function App() {
 
   const setSearchTopStories = (apiResult) => {
     const { hits, page } = apiResult;
-    const oldHits = page !== 0 ? results.hits : [];
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
     const updatedHits = [...oldHits, ...hits];
-    setResults({ hits: updatedHits });
+    console.log(updatedHits);
+    console.log(searchKey);
+    setResults({ ...results, [searchKey]: { hits: updatedHits, page } });
+    console.log(results);
     setPage(page);
   };
   const fetchData = async (page) => {
     setSearchKey(searchTerm);
+    console.log(searchKey);
     return await fetch(
-      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFULT_HPP}`
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchKey}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFULT_HPP}`
     )
       .then((response) => response.json())
       .then((result) => setSearchTopStories(result));
@@ -40,16 +49,26 @@ function App() {
 
   const onDismiss = (id) => {
     const isNotId = (item) => item.objectID !== id;
-    const updatedList = results.hits.filter(isNotId);
-    setResults({ ...results, hits: updatedList });
+    const updatedList = results[searchKey].hits.filter(isNotId);
+    setResults({ ...results, [searchKey]: { hits: updatedList, page } });
   };
   const onSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const needsToSearchTopStories = () => !results[searchTerm];
+
   const onSearchSubmit = (event) => {
-    fetchData(DEFAULT_PAGE);
+    console.log(searchTerm);
+    setSearchKey(searchTerm);
+    console.log(searchKey);
+    if (needsToSearchTopStories()) {
+      fetchData(DEFAULT_PAGE);
+    }
+
     event.preventDefault();
   };
+
   return (
     <div className="page">
       <div className="interactions">
@@ -61,7 +80,7 @@ function App() {
       </div>
       {results && (
         <>
-          <Table list={results.hits} onDismiss={onDismiss} />
+          <Table list={list} onDismiss={onDismiss} />
           <div className="interactions">
             <button onClick={() => fetchData(page + 1)}>More</button>
           </div>
